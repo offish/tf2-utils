ecc88f3fe36383a3f595330fecc2a72438e62585
offish <overutilization@gmail.com>
2019-03-21 16:02:12 +0100

    Update get_all_bots.py

--- tf2automatic/get_all_bots.py
+++ tf2automatic/get_all_bots.py
@@ -9,7 +9,7 @@ r = requests.get('https://api.tf2automatic.com/v1/bots').json()
 if r['success'] == True:
     for i in r['result']:
         data.append(int(i['steamid']))
-    print('All tf2automatic bots were written to {}'.format(file))
+    print('All {} tf2automatic bots were written to {}'.format(len(r['result']), file))
 else:
     print('Error. Wrong response from the API')
 
