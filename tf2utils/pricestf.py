38640def763b0249fb444264d01f28326a9dea53
offish <overutilization@gmail.com>
2019-09-07 12:57:13 +0200

    Update pricestf.py

--- tf2utils/pricestf.py
+++ tf2utils/pricestf.py
@@ -1,5 +1,4 @@
-import requests
-import json
+from tf2utils.methods import request, post
 
 headers = {}
 
@@ -18,14 +17,14 @@ class Prices:
     def get_schema(self) -> dict:
         '''Gets all items recorded'''
 
-        return request(Prices.schema, {}, headers)
+        return request(self.schema, {}, headers)
 
     def get_pricelist(self, src: str, cur: str) -> dict:
         '''Gets all suggested prices\n
         `src` - The source of the prices. bptf, mplc\n
         `cur` - Currency to return the prices in USD, EUR, etc.'''
 
-        url = Prices.items.format('?')
+        url = self.items.format('?')
         params = {'src': src, 'cur': cur.upper()}
         return request(url, params, headers)
 
@@ -35,7 +34,7 @@ class Prices:
         `src` - The source of the prices. bptf, mplc\n
         `cur` - Currency to return the prices in USD, EUR, etc.'''
 
-        url = Prices.items.format(sku) + '?'
+        url = self.items.format(sku) + '?'
         params = {'src': src, 'cur': cur.upper()}
         return request(url, params, headers)
 
@@ -46,10 +45,10 @@ class Prices:
         `sku` - The SKU of the item'''
 
         params = {'src': src, 'cur': cur.upper()}
-        url = Prices.items.format(sku) + '/history?'
+        url = self.items.format(sku) + '/history?'
         return request(url, params, headers)
 
-
+    
 class Snapshots:
 
     items = 'https://api.prices.tf/items/{}'
@@ -65,16 +64,15 @@ class Snapshots:
         '''Requests an item to be priced\n
         `sku` - The SKU of the item'''
 
-        url = Snapshots.items.format(sku)
+        url = self.items.format(sku)
         data = {'source': 'bptf'}
-        r = requests.post(url, data=data, headers=headers)
-        return r.text
+        return post(url, headers=headers, data=data)
 
     def get_snapshot(self, sku: str) -> dict:
         '''Gets most recent snapshot of an item\n
         `sku` - The SKU of the item'''
 
-        url = Snapshots.items.format(sku) + '/snapshot'
+        url = self.items.format(sku) + '/snapshot'
         return request(url, {}, headers)
 
     def get_all_snapshot(self, empty: bool, listings: bool, sku: str) -> dict:
@@ -83,7 +81,7 @@ class Snapshots:
         `listings` - True\n
         `sku` - The SKU of the item'''
 
-        url = Snapshots.items.format(sku) + '/snapshots?'
+        url = self.items.format(sku) + '/snapshots?'
         params = {'empty': empty, 'listings': listings}
         return request(url, params, headers)
 
@@ -91,25 +89,12 @@ class Snapshots:
         '''Gets a single snapshot with listings\n
         `listing_id` - Listing id to search for (5c7c222f3857c355db65f4ee)'''
 
-        url = Snapshots.snapshot.format(listing_id)
+        url = self.snapshot.format(listing_id)
         return request(url, {}, headers)
 
     def request_new_snapshot(self, sku: str) -> dict:
         '''Requests a new snapshot to be taken of an item\n
         `sku` - The SKU of the item'''
 
-        url = Snapshots.items.format(sku) + '/snapshot'
-        r = requests.post(url, headers=headers)
-        return r.text
-
-
-def request(url: str, params: dict, headers: dict) -> dict:
-    r = requests.get(url, params, headers=headers)
-
-    if not r.ok:
-        try:
-            json.loads(r.json)
-            return r.json()
-        except:
-            return {"success": False, "message": f"HTTP error {r.status_code}"}
-    return r.json()
+        url = self.items.format(sku) + '/snapshot'
+        return post(url, headers=headers)
